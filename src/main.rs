8a85528912bfe944b4d2c099b4f96cb91fe13fbb
Lance McCluskey <t.lance.mccluskey@gmail.com>
2022-10-16 06:57:02 -0500

    feat: adding health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{extract::Path, response::IntoResponse, routing::get, Router};
+
+async fn greet(Path(name): Path<String>) -> impl IntoResponse {
+    format!("Hello {name}!")
+}
+
+async fn hello_world() -> &'static str {
+    "Hello World!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/:name", get(greet))
+        .route("/", get(hello_world))
+        .route("/health_check", get(|| async {}));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
