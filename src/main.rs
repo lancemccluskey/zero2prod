cd31b1fa159ce83f3d587c86386f67e919c5b4d1
Lance McCluskey <t.lance.mccluskey@gmail.com>
2022-10-21 14:06:55 -0500

    refactor: splitting into bin and lib and adding tests

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,6 @@
-use std::net::SocketAddr;
-
-use axum::{extract::Path, response::IntoResponse, routing::get, Router};
-
-async fn greet(Path(name): Path<String>) -> impl IntoResponse {
-    format!("Hello {name}!")
-}
-
-async fn hello_world() -> &'static str {
-    "Hello World!"
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/:name", get(greet))
-        .route("/", get(hello_world))
-        .route("/health_check", get(|| async {}));
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Listening on {}", addr);
-
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    run().unwrap().await.unwrap();
 }
