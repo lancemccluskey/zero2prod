3151181bed565db05b89c309092cdffc77dddc80
Lance McCluskey <t.lance.mccluskey@gmail.com>
2022-12-09 05:56:48 -0600

    refactor: updating status migration

--- migrations/20221209114346_add_status_to_subscriptions.sql
+++ migrations/20221209114346_add_status_to_subscriptions.sql
@@ -1,6 +1,3 @@
 -- Add `status` as an optional column
 -- This way we can do a db migration that wont break prod
-ALTER TABLE
-  subscriptions
-ADD
-  COLUMN status TEXT NULL;
+ALTER TABLE subscriptions ADD COLUMN status TEXT NULL;
